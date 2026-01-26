3900e6054a5745677dabd4ea3abc224b506cf1e8
Joaquin Jurado <joaquin.jurado@experienceis.com>
2026-01-26 10:12:25 +0100

    mas

--- src/lib/icons.ts
+++ src/lib/icons.ts
@@ -5,14 +5,19 @@ import { icons as simpleIcons } from "@iconify-json/simple-icons";
 /* ICONOS CENTRALIZADOS                                      */
 /* ========================================================= */
 
-export const wordpress = iconToSVG(getIconData(simpleIcons, "wordpress"), { width: 24 });
-export const github = iconToSVG(getIconData(simpleIcons, "github"), { width: 24 });
-export const php = iconToSVG(getIconData(simpleIcons, "php"), { width: 24 });
-export const mysql = iconToSVG(getIconData(simpleIcons, "mysql"), { width: 24 });
-export const javascript = iconToSVG(getIconData(simpleIcons, "javascript"), { width: 24 });
-export const woocommerce = iconToSVG(getIconData(simpleIcons, "woocommerce"), { width: 24 });
-export const mailchimp = iconToSVG(getIconData(simpleIcons, "mailchimp"), { width: 24 });
-export const googlecalendar = iconToSVG(getIconData(simpleIcons, "googlecalendar"), { width: 24 });
-export const extensions = iconToSVG(getIconData(simpleIcons, "diaspora"), { width: 24 });
-export const integrations = iconToSVG(getIconData(simpleIcons, "ray"), { width: 24 });
-export const performance = iconToSVG(getIconData(simpleIcons, "pagespeedinsights"), { width: 24 });
+export const wordpress = iconToSVG(getIconData(simpleIcons, "wordpress") );
+export const github = iconToSVG(getIconData(simpleIcons, "github") );
+export const php = iconToSVG(getIconData(simpleIcons, "php") );
+export const mysql = iconToSVG(getIconData(simpleIcons, "mysql") );
+export const javascript = iconToSVG(getIconData(simpleIcons, "javascript") );
+export const woocommerce = iconToSVG(getIconData(simpleIcons, "woocommerce") );
+export const mailchimp = iconToSVG(getIconData(simpleIcons, "mailchimp") );
+export const googlecalendar = iconToSVG(getIconData(simpleIcons, "googlecalendar") );
+export const extensions = iconToSVG(getIconData(simpleIcons, "diaspora") );
+export const integrations = iconToSVG(getIconData(simpleIcons, "ray") );
+export const performance = iconToSVG(getIconData(simpleIcons, "pagespeedinsights") );
+export const x = iconToSVG(getIconData(simpleIcons, "nextra") );
+export const behance = iconToSVG(getIconData(simpleIcons, 'behance') );
+export const linkedin = iconToSVG(getIconData(simpleIcons, 'linkedin') );
+export const dev = iconToSVG(getIconData(simpleIcons, 'devbox') );
+export const sitio = iconToSVG(getIconData(simpleIcons, 'inductiveautomation') );
